a76153e185f29eabb6b1529ee33d9b92c7103497
MAC <mac@MacBook-Pro-cua-MAC-3.local>
2025-05-08 09:55:30 +0700

    up demo

---
+++ src/bai_1_introduction_to_java/SystemTime.java
@@ -0,0 +1,11 @@
+package bai_1_introduction_to_java;
+
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now  = new Date(); // Lấy thời gian hiện tại
+
+        System.out.println("Now is : " + now);
+    }
+}
